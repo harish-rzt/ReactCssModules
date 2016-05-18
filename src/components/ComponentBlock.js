4ca15f1386a5521249671318ce3eb3109738a3e7
harish-rzt <harish.v@razorthink.net>
2016-05-18 20:41:14 +0530

    CSS progress

--- src/components/ComponentBlock.js
+++ src/components/ComponentBlock.js
@@ -8,16 +8,16 @@ const ComponentHeader = ({ title, statusColor }) => (
         <h2 className={fLeft + " " + styles.title}>{ title }</h2>
         <Indicator
             className={fRight + " " + styles.indicator}
-            color="red"
-            radius="20"
+            color="green"
+            radius="15"
         />
     </div>
 );
 
 const ComponentProperty = ({ name, value }) => (
     <li className={ styles.item }>
-        <span className={ styles.propName }>{ name }</span>
-        <span className={ styles.propValue }>{ value }</span>
+        <span className={fLeft + " " + styles.propName }>{ name }</span>
+        <span className={fRight + " " + styles.propValue }>{ value }</span>
     </li>
 );
 
@@ -27,7 +27,7 @@ const ComponentBody = ({ properties }) => (
     </ul>
 );
 
-const HR = ({size}) => (<hr className={ComponentHeader} />);
+const HR = ({size}) => (<hr className={styles.line}/>);
 
 export default class ComponentBlock extends React.Component {
 
@@ -36,10 +36,16 @@ export default class ComponentBlock extends React.Component {
     }
 
     render() {
-        return <div className={styles.root}>
+        return <div className={fLeft + " " + styles.root}>
             <ComponentHeader title="ActiveMQ-prod"  statusColor="green"/>
             <HR size="80"/>
-            <ComponentBody properties={ [] }/>
+            <ComponentBody properties={ [
+                {name: "Memory", value: "16gb"},
+                {name: "Memory Used", value: "75%"},
+                {name:"Broker URL", value: "27.0.0.1:3245"},
+                {name: "Uptime", value: "15H"},
+                {name: "Validated", value: "Yes"}
+            ] }/>
         </div>;
     }
 
